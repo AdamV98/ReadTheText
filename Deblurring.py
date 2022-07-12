88c117cc5c8634ccb0f349f5acefb671a2efbc7f
AdamV98 <vagra.adam@gmail.com>
2022-07-12 17:27:49 +0200

    Added: tikhonov method

--- Deblurring.py
+++ Deblurring.py
@@ -2,48 +2,23 @@ import cv2 as cv
 import numpy as np
 import scipy.io as sc
 
-blurred_text = cv.imread("blurred_text.png", cv.IMREAD_GRAYSCALE)
-blurred_after_FFT = np.fft.fft2(blurred_text)
-
 data = sc.loadmat("matrices.mat")
 Ar = data.get("Ar")
 Ac = data.get("Ac")
 B = data.get("B")
 J = data.get("J")
 
-
 h, w = B.shape
-
-B_periodic = np.zeros((h*3, w*3), dtype=np.double)
-for i in range(3):
-    for j in range(3):
-        B_periodic[0+i*h:h+i*h, 0+j*w:w+j*w] = B
-
-
-B_periodic = cv.copyMakeBorder(B, 100, 100, 100, 100, borderType=cv.BORDER_WRAP)
-
-cv.imshow("B_periodic", B_periodic)
 
 matrix = np.zeros((h, w), dtype=np.double)
 matrix[int(matrix.shape[0]/2), int(matrix.shape[1]/2)] = 1
 psf = np.matmul(Ac, matrix)
 psf = np.matmul(psf, np.transpose(Ar))
-
-tmp = np.zeros((h*3, w*3), dtype=np.double)
-tmp[h:2*h, w:2*w] = psf
-tmp2 = cv.copyMakeBorder(psf, 100, 100, 100, 100, borderType=cv.BORDER_CONSTANT, value=0)
 #psf = tmp
-psf = tmp2
-
 
 psf_normalized = cv.normalize(psf, None, 0, 255, cv.NORM_MINMAX)
 psf_normalized = np.uint8(psf_normalized)
 cv.imshow("psf", psf_normalized)
-
-#P = cv.normalize(P, None, 0, 255, cv.NORM_MINMAX)
-#psf = cv.normalize(psf, None, 0, 255, cv.NORM_MINMAX)
-#Ar = cv.normalize(Ar, None, 0, 255, cv.NORM_MINMAX)
-#Ac = cv.normalize(Ac, None, 0, 255, cv.NORM_MINMAX)
 
 def lucy(img, kernel, iterations):
     blurred = img
@@ -52,18 +27,62 @@ def lucy(img, kernel, iterations):
 
     return img
 
+def inverse_filter(B, psf):
+    B_fft = np.fft.fft2(B)
+    psf_fft = np.fft.fft2(psf)
 
-def wiener_filter(img, kernel, SNR):
+    image_fft = B_fft / psf_fft
+
+    image = np.abs(np.fft.ifft2(image_fft))
+
+    return image
+
+def tikhonov(B, Ar, Ac, alpha):
+    Uc, Sc, Vc = np.linalg.svd(Ac)
+    Ur, Sr, Vr = np.linalg.svd(Ar)
+
+    S = np.atleast_2d(Sc).T @ np.atleast_2d(Sr)
+
+    phi = np.divide(np.power(np.abs(S), 2), np.power(np.abs(S), 2)+alpha**2)
+    Sfilt = phi / S
+    tmp = np.matmul(Uc.T, B)
+    tmp = np.matmul(tmp, Ur)
+    tmp2 = tmp * Sfilt
+    Xfilt = np.matmul(Vc.T, tmp2)
+    Xfilt = np.matmul(Xfilt, Vr)
+
+    return Xfilt
+
+
+def wiener_filter(img, kernel, snr):
+    """
+    Applies the Wiener filter according to the given parameters
+
+    Parameters
+    ----------
+        img : ndarray
+            image in numpy array format
+        kernel : ndarray
+            the kernel for the wiener filter
+        snr : int
+            signal to noise ratio
+
+    Returns
+    -------
+        img
+            the return image
+    """
     img = np.fft.fft2(img)
     kernel = np.fft.fft2(kernel)
-    kernel = np.divide(np.conjugate(kernel),(kernel ** 2 + (1/SNR)))
+    kernel = np.divide(np.conjugate(kernel), (kernel ** 2 + (1/snr)))
     img = img * kernel
     img = np.abs(np.fft.ifft2(img))
     return img
 
 
-deblurred_wiener = wiener_filter(B_periodic, psf, 20000)
-deblurred_lucy = lucy(B_periodic, psf, 10)
+deblurred_wiener = wiener_filter(B, psf, 20000)
+deblurred_inverse = inverse_filter(B, psf)
+deblurred_tikhonov = tikhonov(B, Ar, Ac, 0.002)
 h, w = deblurred_wiener.shape
 deblurred1 = deblurred_wiener[int(h / 2):h, int(w / 2):w]
 deblurred2 = deblurred_wiener[int(h / 2):h, 0:int(w / 2)]
@@ -76,18 +95,14 @@ image[int(h/2):h, 0:int(w/2)] = deblurred3
 image[int(h/2):h, int(w/2):w] = deblurred4
 image = cv.normalize(image, None, 0, 255, cv.NORM_MINMAX)
 image = np.uint8(image)
-deblurred_lucy = cv.normalize(deblurred_lucy, None, 0, 255, cv.NORM_MINMAX)
-deblurred_lucy = np.uint8(deblurred_lucy)
+deblurred_inverse = cv.normalize(deblurred_inverse, None, 0, 255, cv.NORM_MINMAX)
+deblurred_inverse = np.uint8(deblurred_inverse)
 image = cv.medianBlur(image, 5)
 cv.imshow("deblurred_wiener", image)
-cv.imshow("deblurred_lucy", deblurred_lucy)
+cv.imshow("deblurred_inverse", deblurred_inverse)
+cv.imshow("deblurred_tikhonov", deblurred_tikhonov)
 
-#cv.imshow("Ar", Ar)
 cv.imshow("B", B)
-cv.imwrite("deblurred.png", image)
-#cv.imshow("J", J)
-#cv.imshow("P", P)
-#cv.imshow("PSF", psf)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
